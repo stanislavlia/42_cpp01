f8232e2e471b437b0d52bc017f435c7af8f3be3c
Stanislav Liashkov <sliashko@z1t6c1.42bangkok.com>
2024-06-15 17:59:23 +0700

    implemented replace of tokens

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -4,18 +4,51 @@
 #include <string>
 
 
-int	main()
+void	replace_pattern(std::string& str,
+					   std::string old_token,
+					    std::string new_token)
 {
-	std::string str = "Hello World! Great to see you!";
+	std::size_t substr_start = str.find(old_token);
 
-	std::string to_replace = "see";
+	if (old_token.empty())
+		return ;
 
-	std::cout << str.find("see") << std::endl;
 
-	str.erase(str.find("see"), str.find("see") + 3);
-	str.insert(str.find("see"), "hello");
-
-	std::cout << str << std::endl;
-
-	return (0);
+	while (substr_start != std::string::npos)
+	{
+		str.erase(substr_start, old_token.length());
+		str.insert(substr_start, new_token);
+		substr_start = str.find(old_token);
+	}
+	
 }
+
+
+
+
+
+
+// void test_replace_pattern(const std::string& test_case,
+// 							 std::string input,
+// 							  std::string old_token,
+// 							   std::string new_token,
+// 							    const std::string& expected) 
+// {
+//     replace_pattern(input, old_token, new_token);
+//     std::cout << test_case << " EXPECTED: " << expected << " GOT: " << input << std::endl;
+// }
+
+// int main() {
+//     test_replace_pattern("Test 1", "hello world", "world", "everyone", "hello everyone");
+//     test_replace_pattern("Test 2", "hello world world", "world", "everyone", "hello everyone everyone");
+//     test_replace_pattern("Test 3", "hello", "world", "everyone", "hello");
+//     test_replace_pattern("Test 4", "aaaaa", "aa", "b", "bba");
+//     test_replace_pattern("Test 5", "test test test", "test", "exam", "exam exam exam");
+//     test_replace_pattern("Test 6", "empty", "", "no", "empty");
+//     test_replace_pattern("Test 7", "replace this and this", "this", "that", "replace that and that");
+//     test_replace_pattern("Test 8", "banana", "na", "NA", "baNANA");
+//     test_replace_pattern("Test 9", "abababab", "ab", "AB", "ABABABAB");
+//     test_replace_pattern("Test 10", "mississippi", "iss", "ISS", "mISSISSippi");
+
+//     return 0;
+// }
