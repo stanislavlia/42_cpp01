a58fe3e54443d8b2dd7291117a677f5211d5ad94
stanislavlia <stanislavliashkov@gmail.com>
2024-06-24 13:03:02 +0700

    Make it as an object of Replacer

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,28 +1,6 @@
 
 #include "Replacer.hpp"
 
-
-
-void	replace_pattern(std::string& str,
-					   std::string old_token,
-					    std::string new_token)
-{
-	std::size_t substr_start = str.find(old_token);
-
-	if (old_token.empty())
-		return ;
-
-
-	while (substr_start != std::string::npos)
-	{
-		str.erase(substr_start, old_token.length());
-		str.insert(substr_start, new_token);
-		substr_start = str.find(old_token);
-	}
-	
-}
-
-
 int main(int argc, char *argv[])
 {
 	if (argc != 4)
